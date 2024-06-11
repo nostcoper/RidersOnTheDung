e9712942dd1de143bcdfa61688f1bd0410a7fded
Michael Estiven Reyes Escobar <maik1234reyes@gmail.com>
2024-06-10 23:59:32 -0500

    Timer

---
+++ RidersOnTheDung/Assets/Scripts/Timmer/timerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class timerScript : MonoBehaviour
+{
+    public TextMeshProUGUI textTimer;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+
+        }
+        else if (time < 0)
+        {
+            time = 0;
+            textTimer.text = "Time is Over";
+            textTimer.color = Color.red;
+
+            SceneManager.LoadScene("GameOver");
+
+        }
+
+
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+
+        textTimer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
