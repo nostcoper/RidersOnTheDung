d1bc551f02042b17025da122d4a3b0f59cb1c55d
nostcoper <carlosserrato200475@gmail.com>
2024-06-12 23:52:31 -0500

    Video intro

--- RidersOnTheDung/Assets/Scripts/DungManager.cs
+++ RidersOnTheDung/Assets/Scripts/DungManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KatamariManager : MonoBehaviour
+public class DungManager : MonoBehaviour
 {
     public GameObject debugCube;
     public GameObject debugSphere;
--- RidersOnTheDung/Assets/Scripts/MenuInicial.cs
+++ RidersOnTheDung/Assets/Scripts/MenuInicial.cs
@@ -7,23 +7,24 @@ using UnityEngine.SceneManagement;
 public class MenuInicial : MonoBehaviour
 {
 
-    public void Jugar(){
-        //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+    public void Volver(){
         SceneManager.LoadScene("Nivel");
     }
 
     public void Instrucciones(){
-        //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
         SceneManager.LoadScene("Instrucciones");
     }
 
     public void Creditos(){
-        //SceneManager.UnloadSceneAsync("menuInicial");
         SceneManager.LoadScene("Creditos");
     }
 
     public void Salir(){
         Application.Quit();
+    }
+
+    public void Jugar(){
+        SceneManager.LoadScene("INTRO");
     }
 
     
